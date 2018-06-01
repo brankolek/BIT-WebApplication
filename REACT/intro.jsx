9d2ebb15c2977d47829a67faeee1e6a3e212da71
BIT student <classroom@bgit.rs>
2018-06-01 15:02:10 +0200

    React

---
+++ REACT/intro.jsx
@@ -0,0 +1,107 @@
+const Content = () => {
+
+    return (
+        <div>
+            <h1> Img </h1> <img src="https://yt3.ggpht.com/a-/ACSszfGArnDnM15PiKvRteGXL27ifxIuRfv_6k2cwQ=s900-mo-c-c0xffffffff-rj-k-no"
+                alt="" />
+        </div>
+    )
+}
+
+
+const Information = () => {
+    return (
+        <p>Butterfly Effect</p>
+    )
+}
+
+
+
+const root = document.getElementById("root")
+ReactDOM.render(< Content />, <Information />, root);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Text = () => {
+//     return (
+
+//         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, fugiat!</p>
+//     )
+// }
+
+
+// const App = () => {
+
+//     return (
+//         <div>
+//             <div> <h1> HELLOOOO </h1></div >
+//             <Text />
+//         </div>
+//     )
+// }
+
+
+
+
+// const root = document.getElementById("root")
+// ReactDOM.render(<App />, root)
